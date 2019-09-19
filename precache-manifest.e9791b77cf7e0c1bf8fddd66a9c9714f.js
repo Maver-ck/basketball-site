e89d669afaa8bf1078a68f657682de75f37b4e44
Yasser Ansari <yasser.ansari@jhc.co.uk>
2019-09-19 14:51:52 +0100

    Updates

---
+++ precache-manifest.e9791b77cf7e0c1bf8fddd66a9c9714f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c5d86e2d440a6096609279c02a32a2",
+    "url": "/basketball-site/index.html"
+  },
+  {
+    "revision": "63660d5a895634fff6ea",
+    "url": "/basketball-site/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9c1aec706bbe3c396a34",
+    "url": "/basketball-site/static/js/2.780031ad.chunk.js"
+  },
+  {
+    "revision": "63660d5a895634fff6ea",
+    "url": "/basketball-site/static/js/main.4997cc81.chunk.js"
+  },
+  {
+    "revision": "aec973d39f9c47e5c6aa",
+    "url": "/basketball-site/static/js/runtime~main.e81a1caa.js"
+  }
+]);
