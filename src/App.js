7d99ff528ed9fd897be4e7cc8d57e3b16ffa29bf
Yasser Ansari <yasser.ansari@jhc.co.uk>
2019-09-19 14:06:36 +0100

    Initial changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Basket Ball</h1>
+      <h2>LA Clippers</h2>
+      <>
+        <h3>Starting Lineup</h3>
+        <div>Center</div>
+        <div>Point Guard</div>
+        <div>Shooting Guard</div>
+        <div>Power Forward</div>
+        <div>Small forward</div>
+      </>
     </div>
   );
 }
