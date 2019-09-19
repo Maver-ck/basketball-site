0d7e3099e54e31700f7fe6cbc16b6a2486c9432b
Yasser Ansari <yasser.ansari@jhc.co.uk>
2019-09-19 16:39:41 +0100

    adding images and centering content of flexbox

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 import "./App.css";
 import ivica from "./72148.png";
+import montrezl from "./montrezl.png";
+import kawhi from "./kawhi.png";
+import pg from "./pg.png";
+import bev from "./bev.png";
 import { Column, Row } from "simple-flexbox";
+
+const PlayerCardColumn = props => (
+  <Column>
+    <div>{props.playerPosition}</div>
+    <div>{props.playerName}</div>
+    <img style={{ alignSelf: "center" }} src={props.playerImage} alt="Logo" />
+  </Column>
+);
 
 function App() {
   return (
@@ -10,16 +22,32 @@ function App() {
       <h2>LA Clippers</h2>
       <div>
         Starting Lineup
-        <Row>
-          <Column>
-            <div>Centerrr</div>
-            <div className="name">Ivica Zubac</div>
-            <img src={ivica} alt="Logo" />
-          </Column>
-          <Column>Point Guard</Column>
-          <Column>Shooting Guard</Column>
-          <Column>Power Forward</Column>
-          <Column>Small forward</Column>
+        <Row style={{ justifyContent: "center" }}>
+          <PlayerCardColumn
+            playerPosition="Center"
+            playerName="Ivica Zubac"
+            playerImage={ivica}
+          ></PlayerCardColumn>
+          <PlayerCardColumn
+            playerPosition="Point Guard"
+            playerName="Patrick Beverley"
+            playerImage={bev}
+          ></PlayerCardColumn>
+          <PlayerCardColumn
+            playerPosition="Shooting Guard"
+            playerName="Paul George"
+            playerImage={pg}
+          ></PlayerCardColumn>
+          <PlayerCardColumn
+            playerPosition="Power Forward"
+            playerName="Montrezl Harrell"
+            playerImage={montrezl}
+          ></PlayerCardColumn>
+          <PlayerCardColumn
+            playerPosition="Small Forward"
+            playerName="Kawhi Leonard"
+            playerImage={kawhi}
+          ></PlayerCardColumn>
         </Row>
       </div>
     </div>
